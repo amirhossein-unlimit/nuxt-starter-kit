8dfa73c163265aa56c388d285760ce66e2bfc1cd
amirhossein-unlimit <amirhossein.unlimited@gmail.com>
2025-10-04 12:49:34 +0330

    feat: add and optimize fonts using @nuxt/fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,50 @@ export default defineNuxtConfig({
   alias: {
     images: fileURLToPath(new URL('./assets/images', import.meta.url)),
   },
+  fonts: {
+    families: [
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-LightD4.woff',
+        weight: 300,
+        style: 'normal',
+      },
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-RegularD4.woff',
+        weight: 400,
+        style: 'normal',
+      },
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-MediumD4.woff',
+        weight: 500,
+        style: 'normal',
+      },
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-DemiBoldD4.woff',
+        weight: 600,
+        style: 'normal',
+      },
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-BoldD4.woff',
+        weight: 700,
+        style: 'normal',
+      },
+      {
+        name: 'IRANSansX',
+        provider: 'local',
+        src: '/fonts/IRANSansX/woff/IRANSansXFaNum-ExtraBoldD4.woff',
+        weight: 800,
+        style: 'normal',
+      },
+    ],
+  },
 });
