0920e770d2fa7b8126dbce3de03af9749c996322
amirhossein-unlimit <amirhossein.unlimited@gmail.com>
2025-10-09 05:46:19 +0330

    chore: add site configs to user guide

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,16 +3,20 @@
 <template>
   <div
     dir="rtl"
-    class="mx-auto flex min-h-screen max-w-fit flex-col items-center justify-center gap-6 pb-16 font-iran-sans-x"
+    class="container mx-auto fc-center/center min-h-screen gap-6 px-4 font-iran-sans-x md:frr-center/center"
   >
-    <NuxtImg src="/logos/nuxt-green-white.svg" width="250" />
+    <div class="fc-center/center gap-2.5">
+      <NuxtImg src="/logos/nuxt-green-white.svg" width="250" />
 
-    <h1 class="text-xl font-bold">
-      پروژه nuxt starter kit آماده استفاده است 😃
-    </h1>
+      <h1 class="w-full text-center text-xl font-bold text-wrap">
+        پروژه nuxt starter kit آماده استفاده است 😃
+      </h1>
+    </div>
 
-    <ul class="fc-start gap-2">
-      <li class="fr-start/start gap-2">
+    <ul class="fc-start gap-4">
+      <li
+        class="fr-start/start gap-2 rounded-lg border border-primary bg-primary/5 p-2"
+      >
         <button
           class="bg-primary50 rounded-lg border border-primary p-1.5"
           type="button"
@@ -20,10 +24,14 @@
           <NuxtIcon name="i-custom-image" class="text-primary" />
         </button>
 
-        آیکون ها در آدرس <span dir="ltr">/app/assets/icons</span> قرار دارند.
+        <p>
+          آیکون ها در آدرس <span dir="ltr">/app/assets/icons</span> قرار دارند.
+        </p>
       </li>
 
-      <li class="fr-start/start gap-2">
+      <li
+        class="fr-start/start gap-2 rounded-lg border border-primary bg-primary/5 p-2"
+      >
         <button
           class="rounded-lg border border-primary bg-primary/5 p-1.5"
           type="button"
@@ -31,9 +39,26 @@
           <NuxtIcon name="i-custom-type" class="text-primary" />
         </button>
 
-        فونت ها در آدرس
-        <span dir="ltr">/public/fonts/IRANSansXFaNum/woff2</span> قرار گرفته
-        اند.
+        <p>
+          فونت ها در پوشه public قرار دارند تا توسط ماژول
+          <span dir="ltr">@nuxt/fonts</span> شناسایی شوند.
+        </p>
+      </li>
+
+      <li
+        class="fr-start/start gap-2 rounded-lg border border-primary bg-primary/5 p-2"
+      >
+        <button
+          class="bg-primary50 rounded-lg border border-primary p-1.5"
+          type="button"
+        >
+          <NuxtIcon name="i-custom-image" class="text-primary" />
+        </button>
+
+        <p>
+          تنظیمات ثابت پروژه در آدرس <span dir="ltr">/app/constants</span> وجود
+          دارد.
+        </p>
       </li>
     </ul>
   </div>
