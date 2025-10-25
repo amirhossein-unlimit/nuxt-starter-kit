7f6ba0b52b9a52c4eec7141cdedea51ab7de6fb2
amirhossein-unlimit <amirhossein.unlimited@gmail.com>
2025-10-25 11:06:58 +0330

    feat: wrap NuxtPage in NuxtLayout and improve styling in index.vue

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,8 @@
   <NuxtRouteAnnouncer />
 
   <UApp>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </UApp>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,61 +3,54 @@ const { logo } = useAppConfig().site;
 </script>
 
 <template>
-  <div
-    class="container mx-auto fc-center/center min-h-screen gap-6 px-4 font-iran-sans-x md:fr-center/center"
-  >
+  <div class="container mx-auto fc-center/center min-h-screen gap-6 px-4 font-iran-sans-x md:fr-center/center">
     <div class="fc-center/center gap-2.5">
-      <NuxtImg :src="logo" width="250" />
+      <NuxtImg width="250" :src="logo" alt="nuxt logo" />
 
-      <h1 class="w-full text-center text-xl font-bold text-wrap">
+      <h1 class="w-full text-center text-xl font-bold text-wrap text-gray-300">
         The nuxt-starter-kit project is ready to use 😃
       </h1>
     </div>
 
     <ul class="fc-start gap-4">
-      <li
-        class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2"
-      >
-        <button
-          class="bg-primary50 rounded-lg border border-primary p-1.5"
-          type="button"
-        >
-          <NuxtIcon name="i-custom-image" class="text-primary" />
+      <li class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2">
+        <button class="p-1" type="button">
+          <NuxtIcon name="i-custom-image" class="fill-transparent text-white" :size="24" />
         </button>
 
-        <p>The icons are located at /app/assets/icons.</p>
+        <p class="text-gray-300">
+          The icons are located at /app/assets/icons.
+        </p>
       </li>
 
-      <li
-        class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2"
-      >
-        <button
-          class="rounded-lg border border-primary bg-primary/5 p-1.5"
-          type="button"
-        >
-          <NuxtIcon name="i-custom-type" class="text-primary" />
+      <li class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2">
+        <button class="p-1" type="button">
+          <NuxtIcon name="i-custom-type" class="fill-transparent text-white" :size="24" />
         </button>
 
-        <p>
+        <p class="text-gray-300">
           The fonts are placed in the public directory to be recognized by the
           @nuxt/fonts module.
         </p>
       </li>
 
-      <li
-        class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2"
-      >
-        <button
-          class="bg-primary50 rounded-lg border border-primary p-1.5"
-          type="button"
-        >
-          <NuxtIcon name="i-custom-image" class="text-primary" />
+      <li class="fr-start/center gap-2 rounded-lg border border-primary bg-primary/5 p-2">
+        <button class="p-1" type="button">
+          <NuxtIcon name="i-custom-settings" class="fill-transparent text-white" :size="24" />
         </button>
 
-        <p>
+        <p class="text-gray-300">
           The project’s constant configurations are located at /app/constants.
         </p>
       </li>
     </ul>
   </div>
 </template>
+
+<style>
+@reference "~/assets/css/tailwind.css";
+
+body {
+  @apply bg-gray-800;
+}
+</style>
